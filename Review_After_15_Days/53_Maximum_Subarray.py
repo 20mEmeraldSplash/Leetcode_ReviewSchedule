2894518f49b5c77e71f9074ea353b25ae7c115a0
20mEmeraldSplash <yichenwaa@gmail.com>
2024-09-19 05:53:57 -0400

    09/19/2024

--- Review_After_15_Days/53_Maximum_Subarray.py
+++ Review_After_15_Days/53_Maximum_Subarray.py
@@ -6,10 +6,12 @@ class Solution(object):
         """
         max_sum = nums[0]
         current_sum = nums[0]
-        for i in range (1, len(nums)):
-            current_sum = max(nums[i], current_sum+nums[i]) # 这里很厉害
+        for i in range(1, len(nums)):
+            num = nums[i]
+            current_sum = max(current_sum + num, num)
             max_sum = max(max_sum, current_sum)
-        return max_sum
+
+        return max(max_sum, current_sum)
     
 # 时间复杂度: O(n)，只需要遍历数组一次。
 # 空间复杂度: O(1)，只使用了常数的额外空间来保存 current_sum 和 max_sum。
--- Review_After_5_Days/98_Validate_Binary_Search_Tree.py
+++ Review_After_5_Days/98_Validate_Binary_Search_Tree.py
@@ -10,10 +10,10 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        def helper(node, low, high):
-            if not node:
+        def helper(node, small, large):
+            if not node: 
                 return True
-            if not (low < node.val < high):
+            if not (small < node.val < large):
                 return False
-            return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+            return helper(node.left, small, node.val) and helper(node.right, node.val, large)
         return helper(root, float('-inf'), float('inf'))
