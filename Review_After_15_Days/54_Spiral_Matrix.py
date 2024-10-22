6edc28663ea74445ae245dcdc2b40f669c1d7b13
20mEmeraldSplash <yichenwaa@gmail.com>
2024-10-21 21:37:08 -0400

    10/21/2024

--- Review_After_15_Days/54_Spiral_Matrix.py
+++ Review_After_15_Days/54_Spiral_Matrix.py
@@ -4,29 +4,29 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        h = len(matrix)
-        w = len(matrix[0])
         result = []
-        dir_list = [(0, 1), (1, 0), (0,-1), (-1, 0)]
-        dir_index = 0
         checked = []
+        m = len(matrix)
+        n = len(matrix[0])
+        start_m = 0
+        start_n = 0
 
-        i = 0
-        j = 0
-        while len(result) < h * w :
+        dect_list = [(0, 1), (1,0), (0,-1),(-1,0)]
 
-            result.append(matrix[i][j])
-            checked.append((i,j))
+        dect_index = 0
 
-            next_i = i + dir_list[dir_index][0]
-            next_j = j + dir_list[dir_index][1]
+        while len(result) < m * n:
+            if (start_m, start_n) not in checked:
+                result.append(matrix[start_m][start_n])
+                checked.append((start_m, start_n))
+                new_m = start_m + dect_list[dect_index][0]
+                new_n = start_n + dect_list[dect_index][1]
+                if 0<=new_m<m and 0<=new_n<n and (new_m, new_n) not in checked:
+                    start_m = new_m
+                    start_n = new_n
+                else:
+                    dect_index = (dect_index + 1) % 4
+                    start_m = start_m + dect_list[dect_index][0]
+                    start_n = start_n + dect_list[dect_index][1]
+        return result
 
-            if 0 <= next_i < h and 0 <= next_j < w and (next_i, next_j) not in checked:
-                i = next_i
-                j = next_j
-            else:
-                dir_index = (dir_index + 1) % 4
-                i += dir_list[dir_index][0]
-                j += dir_list[dir_index][1]
-
-        return result 
