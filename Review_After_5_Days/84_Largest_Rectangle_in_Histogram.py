48f54700ef465ce7e080e661d47f694e89320ec6
20mEmeraldSplash <yichenwaa@gmail.com>
2024-10-27 17:00:15 -0400

    10/27/24

--- Review_After_5_Days/84_Largest_Rectangle_in_Histogram.py
+++ Review_After_5_Days/84_Largest_Rectangle_in_Histogram.py
@@ -5,18 +5,14 @@ class Solution(object):
         :rtype: int
         """
         heights.append(0)
+        checked = []
         result = 0
-        checked = [0]
-        
         for i in range(len(heights)):
-            if heights[i] >= heights[checked[-1]]:
-                checked.append(i)
-            else:
-                while checked and heights[i] < heights[checked[-1]]:
-                    h = heights[checked.pop(-1)]
-                    w = i if not checked else i - checked[-1] - 1
-                    result = max(result, h * w)
-                checked.append(i)
-        return result
-
+            while checked and heights[i] < heights[checked[-1]]:
+                check_index = checked.pop()
+                h = heights[check_index]
+                w = i - checked[-1] - 1 if checked else i
+                result = max(result, h * w)
+                
+            checked.append(i)
         return result
