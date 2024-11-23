75c959f187aaf34862ad35a693a90f2263f1117d
20mEmeraldSplash <yichenwaa@gmail.com>
2024-11-23 14:42:20 -0500

    11/23/2024

--- Review_After_30_Days/704_Binary_Search.py
+++ Review_After_30_Days/704_Binary_Search.py
@@ -6,21 +6,16 @@ class Solution(object):
         :rtype: int
         """
         def helper(start, end):
-            if start == end:
-                if nums[start] == target:
-                    return start
-                else:
-                    return -1
-            elif start > end:
+            if start > end:
                 return -1
-            else: 
-                mid = (start + end) // 2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    return helper(start, mid)
-                else:
-                    return helper(mid+1, end)
-        return helper(0, len(nums)-1)
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                return helper(mid + 1, end)
+            else:
+                return helper(start, mid - 1)
+
+        return helper(0, len(nums) - 1)
 # 时间复杂度为 O(log n)，其中 n 是数组 nums 的长度。
 # 最多会递归 log₂(n) 次，因此需要 O(log n) 的空间用于存储递归调用的栈帧。
